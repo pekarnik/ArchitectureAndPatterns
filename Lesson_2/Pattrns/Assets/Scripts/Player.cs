2435f14f96619ac6454623b0f6085be72999b417
Pekarnik <pekgoglo@gmail.com>
2020-11-08 19:48:15 +0300

    Create Project

---
+++ Lesson_2/Pattrns/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Asteroids
+{
+    internal sealed class Player : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _acceleration;
+        [SerializeField] private DetectCollision _detectCollision;
+        [SerializeField] private Rigidbody2D _bullet;
+        [SerializeField] private Transform _barrel;
+        [SerializeField] private float _force;
+        private Camera _camera;
+        private Ship _ship;
+
+        private void Start()
+        {
+            _camera = Camera.main;
+            var moveTransform = new AccelerationMove(transform, _speed, _acceleration);
+            var rotation = new RotationShip(transform);
+            _ship = new Ship(moveTransform, rotation);
+            
+            _detectCollision.InjectDependencies(new Health(100));
+        }
+
+        private void Update()
+        {
+            var direction = Input.mousePosition - _camera.WorldToScreenPoint(transform.position);
+            _ship.Rotation(direction);
+            
+            _ship.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                _ship.AddAcceleration();
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                _ship.RemoveAcceleration();
+            }
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                var temAmmunition = Instantiate(_bullet, _barrel.position, _barrel.rotation);
+                temAmmunition.AddForce(_barrel.up * _force);
+            }
+        }
+    }
+}
